193b0f710690ee321293bba4be1b60deb7bedd53
SimTera <vmunerav@icloud.com>
2024-09-03 17:10:49 +0200

    Initial Commit

---
+++ ComicDesk/ComicDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicDeskApp.swift
+//  ComicDesk
+//
+//  Created by Victor Munera on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ComicDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
