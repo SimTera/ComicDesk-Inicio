c3dbaa828cea8abe4a1e03fcafdb153f6c64e3c9
SimTera <vmunerav@icloud.com>
2024-09-29 12:11:49 +0200

    Quitando comentarios y codigo comentado del Comics interactor

--- ComicDesk/ComicsInteractor.swift
+++ ComicDesk/ComicsInteractor.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 protocol ComicsInteractorProtocol {
+    
     func fetchComics(page: Int) async throws -> [ComicModel]
     func searchOnStore(page: Int, text: String) async throws -> [ComicModel]
     func loadComicsByTheme(page: Int, text: String) async throws -> [ComicModel]
     func loadComicsByGenre(page: Int, text: String) async throws -> [ComicModel]
     func loadComicsByDemographic(page: Int, text: String) async throws -> [ComicModel]
-    
 }
 
 struct ComicsInteractor: ComicsInteractorProtocol {
@@ -31,6 +31,7 @@ extension ComicsInteractor {
         let (data, _) = try await URLSession.shared.data(from: .comicSearchContainsURL(page: page).appending(path: text))
         return try JSONDecoder().decode(ComicDataDTO.self, from: data).items.map(\.mapToComicModel)
     }
+    
 //    TODO: Comprobar si esta cargando correctamente la url, crear un let con url y generar la url para poder ver en impresion que me esta generando y como hacer para crearla bien - DONE
     func loadComicsByTheme(page: Int, text: String) async throws -> [ComicModel] {
 //        let url = URL.comicForThemeURL(page: page).appending(path: text) //Para comprobar el fallo de url, era que estaba llamando al listado de generos en vez de a los mangas por generos
@@ -39,6 +40,7 @@ extension ComicsInteractor {
 //        print("Response Data: \(String(data: data, encoding: .utf8) ?? "faild decoding data")")// no me esta generando la url de manera correcta
         return try JSONDecoder().decode(ComicDataDTO.self, from: data).items.map(\.mapToComicModel)
     }
+    
     //TODO: He de crear aqui los interactors de genre y demographic
     func loadComicsByGenre(page: Int, text: String) async throws -> [ComicModel] {
         let (data, _) = try await URLSession.shared.data(from: .comicForGenresURL(page: page).appending(path: text))
@@ -67,22 +69,9 @@ struct ComicListInteractorTest: ComicsInteractorProtocol {
     
     func fetchComics(page: Int) async throws -> [ComicModel] {
         
-        guard let url = Bundle.main.url(forResource: "ComicTestPaginated", withExtension: "json") else { return [] } //creamos la url de dipositivo
-        let data = try Data(contentsOf: url) // cargamos los datos
+        guard let url = Bundle.main.url(forResource: "ComicTestPaginated", withExtension: "json") else { return [] }
+        let data = try Data(contentsOf: url)
         
-        return try JSONDecoder().decode(ComicDataDTO.self, from: data).items.map(\.mapToComicModel) //Decodificamos los datos
-        
+        return try JSONDecoder().decode(ComicDataDTO.self, from: data).items.map(\.mapToComicModel)
     }
 }
-
-//    func searchOnStore(text: String) async throws -> ComicDataDTO {
-//        let url = URL.comicSearchContainsURL.appending(path: text)
-//        print(url)
-//        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-//        components.queryItems = [
-//            URLQueryItem(name: "name", value: "dragon")
-//        ]
-//        let request = URLRequest(url: components.url!)
-//        let (data, _) = try await URLSession.shared.data(for: request)
-//        return try JSONDecoder().decode(ComicDataDTO.self, from: data)
-//    }
